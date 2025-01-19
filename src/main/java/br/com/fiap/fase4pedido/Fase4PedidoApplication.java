0d83af3b03d795475115d2cc962b448e821163ea
Renan Leite <renan.carli@hotmail.com>
2025-01-19 20:56:57 -0300

    First Commit

---
+++ src/main/java/br/com/fiap/fase4pedido/Fase4PedidoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fase4pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fase4PedidoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fase4PedidoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/fase4pedido/Fase4PedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fase4pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fase4PedidoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
